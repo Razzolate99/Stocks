9e5ae69faf328064f0eb97c1913b05f91c3915fe
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-12-04 07:48:49 -0500

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Aaron Ward on 2022-12-04.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
